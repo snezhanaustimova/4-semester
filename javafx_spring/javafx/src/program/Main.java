f1623aa16c0c2d7b888c6095039b8eb2b8a2e769
snezhanaustimova <59029577+snezhanaustimova@users.noreply.github.com>
2021-03-29 02:20:49 +0300

    Add files via upload

---
+++ javafx_spring/javafx/src/program/Main.java
@@ -0,0 +1,120 @@
+package program;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import program.controllers.PersonEditingController;
+import program.controllers.PersonOverviewController;
+import program.models.Person;
+import program.utils.RestAPI;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+    private RestAPI apiSession;
+
+    public Main() {
+        apiSession = new RestAPI();
+        this.updateTable();
+    }
+
+    public void updateTable() {
+        personData.clear();
+        personData.addAll(apiSession.getPersons());
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Address Application");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("views/root.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showPersonOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("views/main.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+            rootLayout.setCenter(personOverview);
+
+            PersonOverviewController controller = loader.getController();
+            controller.setMain(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("views/personEditDialog.fxml"));
+            AnchorPane personEditDialog = (AnchorPane) loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(personEditDialog);
+            dialogStage.setScene(scene);
+
+            PersonEditingController controller = loader.getController();
+            controller.setDialogStage(dialogStage, this, apiSession);
+            controller.setPerson(person);
+
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public BorderPane getRootLayout() {
+        return rootLayout;
+    }
+
+    public RestAPI getApiSession() {
+        return apiSession;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ javafx_spring/javafx/src/program/utils/RestAPI.java
@@ -0,0 +1,53 @@
+package program.utils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import program.models.Person;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestAPI {
+    private static final String SERVER_URL = "http://127.0.0.1:8080";
+    private static final String SERVER_GET_PERSONS = SERVER_URL + "/api/persons";
+
+    public void createPerson(Person person) {
+        HttpClass.postRequest(SERVER_GET_PERSONS, person.toJson());
+    }
+
+    public List<Person> getPersons() {
+        List<Person> result = new ArrayList<>();
+        String buffer = HttpClass.getRequest(SERVER_GET_PERSONS);
+        JsonArray jsonResult = JsonParser.parseString(buffer).getAsJsonArray();
+
+        for (int i = 0; i < jsonResult.size(); i++) {
+            JsonObject currentPerson = jsonResult.get(i).getAsJsonObject();
+            String firstName = currentPerson.get("firstName").getAsString();
+            String lastName = currentPerson.get("lastName").getAsString();
+            String city = currentPerson.get("city").getAsString();
+            String street = currentPerson.get("street").getAsString();
+            Integer postalCode = currentPerson.get("postalCode").getAsInt();
+            LocalDate birthday = LocalDate.parse(currentPerson.get("birthday").getAsString());
+            Integer id = currentPerson.get("id").getAsInt();
+
+            Person newPerson = new Person(firstName, lastName, city, street, postalCode, birthday, id);
+            result.add(newPerson);
+        }
+        return result;
+    }
+
+    public void updatePerson(Person person) {
+        Integer id = person.getId();
+        String jsonString = person.toJson();
+        HttpClass.putRequest(SERVER_GET_PERSONS + "/" + id, jsonString);
+    }
+
+    public boolean deletePerson(Person person) {
+        Integer id = person.getId();
+        if (id == null)
+            return false;
+        return HttpClass.deleteRequest(SERVER_GET_PERSONS + "/" + id);
+    }
+}
