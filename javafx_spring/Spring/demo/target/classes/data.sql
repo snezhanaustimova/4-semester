d76b4c84b51ccc88812b036a08486caeb917fd09
snezhanaustimova <59029577+snezhanaustimova@users.noreply.github.com>
2021-03-29 12:04:22 +0300

    Delete javafx_spring/Spring/demo directory

--- javafx_spring/Spring/demo/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO person (first_name, last_name, city, street, postal_code, birthday) VALUES
-  ('Aliko', 'Dangote', 'Moscow', 'Prospect Mirs', 485748, '2020-03-22'),
-  ('Bill', 'Gates', 'Paris', '4th avenu', 492044, '2020-03-22'),
-  ('Folrunsho', 'Alakija', 'Tokyo', 'Main Street', 234345, '2020-03-22');
